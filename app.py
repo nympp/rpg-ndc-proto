bcc0eb8ddf0a5b4d3a49e9051f7f6f554e619866
nymp <nympha245@gmail.com>
2026-04-24 23:10:17 +0200

    comment print inutiles

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def draw():
         enemy_1_y = game_map.read_collision_map(current_collision_map)[0][0][3]
 
         game_enemies.draw_enemy(enemy_1_id, enemy_1_x, enemy_1_y) # Afficher l'ennemi d'ID et aux coordonées précedemment récupérés
-        print("Enemy 1 detects Player :", game_enemies.is_player_in_enemy_line(game_map.read_collision_map(current_collision_map)[0][0][4], current_collision_map))
+       # print("Enemy 1 detects Player :", game_enemies.is_player_in_enemy_line(game_map.read_collision_map(current_collision_map)[0][0][4], current_collision_map))
     
 
         if not game_map.read_collision_map(current_collision_map)[0][1] == []: # Même logique que pour les tests précédents, mais avec les ennemis 2 puis 3
@@ -74,7 +74,7 @@ def draw():
             enemy_2_y = game_map.read_collision_map(current_collision_map)[0][1][3]
 
             game_enemies.draw_enemy(enemy_2_id, enemy_2_x, enemy_2_y)
-            print("Enemy 2 detects Player :", game_enemies.is_player_in_enemy_line(game_map.read_collision_map(current_collision_map)[0][1][4], current_collision_map))
+        #    print("Enemy 2 detects Player :", game_enemies.is_player_in_enemy_line(game_map.read_collision_map(current_collision_map)[0][1][4], current_collision_map))
 
             if not game_map.read_collision_map(current_collision_map)[0][2] == []:
                 
@@ -83,6 +83,6 @@ def draw():
                 enemy_3_y = game_map.read_collision_map(current_collision_map)[0][2][3]
 
                 game_enemies.draw_enemy(enemy_3_id, enemy_3_x, enemy_3_y)
-                print("Enemy 3 detects Player :", game_enemies.is_player_in_enemy_line(game_map.read_collision_map(current_collision_map)[0][2][4], current_collision_map))
+             #   print("Enemy 3 detects Player :", game_enemies.is_player_in_enemy_line(game_map.read_collision_map(current_collision_map)[0][2][4], current_collision_map))
 
 pyxel.run(update,draw)
